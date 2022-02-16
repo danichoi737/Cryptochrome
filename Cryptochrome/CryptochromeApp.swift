8fcaa9d174b61fc4066e2303b9657d23205ad635
danichoi737 <danichoi737@gmail.com>
2022-02-16 22:47:53 +0900

    Initial Commit

---
+++ Cryptochrome/CryptochromeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptochromeApp.swift
+//  Cryptochrome
+//
+//  Created by Hyunwook CHOI on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct CryptochromeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
