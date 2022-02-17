0b5d170658823a775abd3f0144799251974cd080
danichoi737 <danichoi737@gmail.com>
2022-02-17 21:08:06 +0900

    Add TabView

---
+++ Cryptochrome/DashboardView.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardView.swift
+//  Cryptochrome
+//
+//  Created by Hyunwook CHOI on 2022/02/17.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text("DashboardView")
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
---
+++ Cryptochrome/LocationView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationView.swift
+//  Cryptochrome
+//
+//  Created by Hyunwook CHOI on 2022/02/17.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text("LocationView")
+    }
+}
+
+struct LocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationView()
+    }
+}
